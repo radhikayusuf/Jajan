2047fb3d1646fce23364da9381060b81d2027c9e
aqiel <maqiel2600@gmail.com>
2016-10-18 14:57:23 +0700

    fix detail activity

--- app/src/main/java/com/pkl/gits/jajan/Main/Fragment/ViewModel/HomeFragmentVM.java
+++ app/src/main/java/com/pkl/gits/jajan/Main/Fragment/ViewModel/HomeFragmentVM.java
@@ -79,7 +79,5 @@ public class HomeFragmentVM extends GitsVM{
 
             background.addSlider(textSliderView);
         }
-
-
     }
 }
--- app/src/main/java/com/pkl/gits/jajan/Main/RecyclerViewSetting/BestSeller/BestSellerAdapter.java
+++ app/src/main/java/com/pkl/gits/jajan/Main/RecyclerViewSetting/BestSeller/BestSellerAdapter.java
@@ -1,11 +1,9 @@
 package com.pkl.gits.jajan.Main.RecyclerViewSetting.BestSeller;
 
 import android.content.Intent;
-import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
-import com.pkl.gits.jajan.Main.DetailBarang.DetailActivity;
+import com.pkl.gits.jajan.DetailBarang.DetailActivity;
 import com.pkl.gits.jajan.R;
 import com.pkl.gits.jajan.api.Response.BarangResponse;
 import com.pkl.gits.jajan.databinding.RowBestSellerBinding;
@@ -13,7 +11,6 @@ import com.pkl.gits.jajan.databinding.RowBestSellerBinding;
 import java.util.List;
 
 import id.gits.mvvmcore.adapter.GitsAdapter;
-import id.gits.mvvmcore.viewmodel.GitsRowVM;
 
 /**
  * Created by root on 17/10/16.
